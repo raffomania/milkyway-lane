7bc2b6bbc39186b338aaf906dc8f365ee19c42c3
Rafael Epplée <hello@rafa.ee>
2025-04-06 14:56:04 +0200

    Second day

--- planet.gd
+++ planet.gd
@@ -1,4 +1,16 @@
+class_name Planet
+
 extends Sprite2D
+
+var radius := 50.0
+@export
+var target: Planet:
+    set(val):
+        target = val
+        if val == self:
+            push_warning(val, " has itself as target")
 
 func _ready() -> void:
     add_to_group("planets")
+
+    radius = texture.get_size().x * scale.x * .5
--- ship.gd
+++ ship.gd
@@ -1,23 +1,34 @@
 extends Sprite2D
 
 @export
-var target: Node2D
-var max_speed = 50.0
+var target: Planet
+var max_velocity = 150.0
+var min_speed = 5.0
 var velocity := 0.0
-var acceleration := 10.0
-
+var max_acceleration := 50.0
 
 func _process(delta: float) -> void:
-	var to_target = target.global_position - global_position
+	if !is_instance_valid(target):
+		return
+
+	var target_position = target.global_position + (Vector2.RIGHT * target.radius * 1.1)
+	var to_target = target_position - global_position
 	var distance_to_target = to_target.length()
 
-	if distance_to_target > 100:
-		velocity += acceleration * delta
-	else:
-		velocity -= acceleration * delta
-
-	velocity = clamp(velocity, 0, max_speed)
-
 	var target_rotation = to_target.angle() + PI / 2
-	rotation = rotate_toward(global_rotation, target_rotation, delta)
+	var rotation_strength = abs(angle_difference(target_rotation, global_rotation)) * delta * 2
+
+	var braking_distance = (velocity ** 2) / (2 * max_acceleration)
+	var should_accelerate = 1 if distance_to_target > braking_distance else -1
+	var acceleration_strength = should_accelerate / rotation_strength
+	var acceleration = clamp(max_acceleration * acceleration_strength, -max_acceleration, max_acceleration)
+
+	velocity += acceleration * delta
+	
+	velocity = clamp(velocity, min_speed, max_velocity)
+
+	rotation = rotate_toward(global_rotation, target_rotation, rotation_strength)
 	position += velocity * Vector2.UP.rotated(rotation) * delta
+
+	if distance_to_target < 30:
+		target = target.target
