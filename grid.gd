28ce11f433d18cdd8c1cee4518f2fc036fba5238
Rafael Epplée <hello@rafa.ee>
2026-04-01 17:09:39 +0200

    make spawning bounds explicit

---
+++ grid.gd
@@ -0,0 +1,4 @@
+extends Sprite2D
+
+@onready
+var global_bounds := Rect2(global_position - scale * 0.5, scale)
--- planet_spawner.gd
+++ planet_spawner.gd
@@ -3,6 +3,9 @@ extends Node
 @onready
 var home_planet = $'../1/Home'
 
+@onready
+var grid = $'../Grid'
+
 func _input(event: InputEvent) -> void:
     if event.is_action_released('ui_up'):
         spawn()
@@ -35,22 +38,21 @@ func new_planet_position(radius: float) -> Variant:
     # Take the viewport bounds and add a little margin so planets
     # don't spawn at the edge of the screen
     var bounds_margin = Vector2.ONE * 120
-    var bounds = get_viewport().get_visible_rect()
-    bounds.position = bounds.position + bounds_margin
-    bounds.size = bounds.size - bounds_margin * 2
+    var position = grid.global_bounds.position + bounds_margin
+    var size = grid.global_bounds.size - bounds_margin * 2
 
     var planets = get_tree().get_nodes_in_group("planets")
 
     # Try to randomly find a position on the map
     # where no planet exists yet
     for try in range(50_000):
-        var offset = Vector2(randi_range(0, bounds.size.x), randi_range(0, bounds.size.y))
-        var position = bounds.position + offset
+        var offset = Vector2(randi_range(0, size.x), randi_range(0, size.y))
+        var random_position = position + offset
 
         # If position is in an empty space on the map, return it
-        var collides = planets.any(func(planet): return planet.position.distance_to(position) < radius + planet.radius + 70)
+        var collides = planets.any(func(planet): return planet.position.distance_to(random_position) < radius + planet.radius + 70)
         if not collides:
-            return position
+            return random_position
 
     print("can't find new planet position, giving up")
     return null
